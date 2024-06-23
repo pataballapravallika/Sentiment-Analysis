dd93ea46095073ad64cda4e6751371362b009d71
Pravallika Pataballa <154676959+pataballapravallika@users.noreply.github.com>
2024-06-23 20:54:46 +0530

    Create sentimentAnalysis.py

---
+++ sentimentAnalysis.py
@@ -0,0 +1,113 @@
+import pandas as pd
+import os
+os.chdir("C://Users//hp//Downloads")
+data = pd.read_csv('Reviews(1).csv')
+data
+data.head()
+data.tail()
+data.info()
+data.describe()
+data.isnull().sum()
+data.duplicated()
+value_counts = data['Liked'].value_counts()
+print(value_counts)
+import matplotlib.pyplot as plt
+import seaborn as sns
+value_counts.plot(kind = 'bar' , color = ['blue', 'green'])
+plt.title("Sentiment value counts")
+plt.xlabel('Liked')
+plt.ylabel('Count')
+plt.xticks(ticks=[0,1] , labels=['Postive','Negative'],rotation=0)
+plt.show()
+from wordcloud import WordCloud
+combined_text = " ".join(data['Review'])
+wordcloud = WordCloud(width = 800 , height = 400 ,background_color = 'white').generate(combined_text)
+plt.figure(figsize=(10,6))
+plt.imshow(wordcloud, interpolation = 'bilinear')
+plt.axis('off')
+plt.title('Word Cloud of Reviews')
+plt.show()
+from collections import Counter
+target_words = ['food','place','restaurant']
+all_words = " ".join(data['Review']).lower().split()
+word_counts = Counter(all_words)
+target_word_counts = {word:word_counts[word] for word in target_words}
+plt.figure(figsize=(8,6))
+plt.bar(target_word_counts.keys(),target_word_counts.values() , color = ['blue','green','orange'])
+plt.xlabel('words')
+plt.ylabel('Frequenecy')
+plt.title('Frequency of specific words in Reviews')
+plt.show()
+lowercased_text = data['Review'].str.lower()
+from nltk.tokenize import word_tokenize
+data['Tokens'] = data['Review'].apply(word_tokenize)
+print(data['Tokens'])
+data.info()
+data['Review'].value_counts()
+import string
+data['Review'] = data['Review'].str.replace(f"[{string.punctuation}]"," ",regex = True)
+print(data['Review'])
+data['Review'].value_counts()
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+data['Tokens'] = data['Review'].apply(lambda x: [word for word in word_tokenize(x) if word not in stop_words])
+print(data['Tokens'])
+#Stemming
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+stemmer = PorterStemmer()
+data['stemmed'] = data['Review'].apply(lambda x: ' '.join([stemmer.stem(word) for word in word_tokenize(x)]))
+print(data['stemmed'])
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet
+lemmatizer = WordNetLemmatizer()
+data['Lemmatized'] = data['Review'].apply(lambda x :' '.join([lemmatizer.lemmatize(word , pos = wordnet.VERB) for word in word_tokenize(x)]))
+print(data['Lemmatized'])
+#Removing the numbers from reviews
+import re
+data['No_Numbers'] = data['Review'].apply(lambda x : re.sub(r'\d+',' ' ,x))
+print(data['No_Numbers'])
+#removing special characters like @ # %,*
+data['cleaned'] = data['Review'].apply(lambda x: re.sub(r'[^A-Za-z0-9\s]','' ,x))
+print(data['cleaned'])
+import contractions
+data['Expanded'] = data['Review'].apply(contractions.fix)
+print(data['Expanded'])
+import emoji
+data['emoji'] = data['Review'].apply(emoji.demojize)
+print(data['emoji'])
+from bs4 import BeautifulSoup
+data['cleaned'] = data['Review'].apply(lambda x: BeautifulSoup(x,"html.parser").get_text())
+print(data['cleaned'])
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(data['Review'])
+print(X.toarray())
+#bulding a machine learning model
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score,classification_report
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(data['Review'])
+y = data['Liked']
+X_train,X_test,y_train,y_test = train_test_split(X,y , test_size = 0.2 , random_state = 42)
+model = MultinomialNB() 
+model.fit(X_train,y_train)
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test,y_pred)
+report = classification_report(y_test,y_pred)
+print(f'Accuracy {accuracy}')
+print(f'Classfication Report:')
+print(report)
+print(f'Classfication Report:')
+print(report)
+#new review prediction
+new_review = input("Enter a review")
+cleaned_review = preprocess_review(new_review)
+new_review_vectorized = vectorizer.transform([cleaned_review])
+prediction = model.predict(new_review_vectorized)
+if prediction[0] == 1:
+    print("The review is predicted postive")
+else:
+    print("The review is predicted negative")
